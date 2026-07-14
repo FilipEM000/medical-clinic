20ada7aeb37b8684a888e8b1e34b48fca3a0d299
Filip <filip.ostrowicki@wp.pl>
2026-07-14 21:19:21 +0200

    initial-commit

---
+++ src/test/java/com/FilipEM000/medical_clinic/MedicalClinicApplicationTests.java
@@ -0,0 +1,13 @@
+package com.FilipEM000.medical_clinic;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MedicalClinicApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
